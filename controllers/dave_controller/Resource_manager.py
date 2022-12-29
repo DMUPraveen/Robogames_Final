6fd1694914d42929db1c9b46d1dcc2173204e89a
DMUPraveen <dmu.praveen@gmail.com>
2022-12-29 17:49:56 +0530

    addded epuck updater

--- controllers/dave_controller/Resource_manager.py
+++ controllers/dave_controller/Resource_manager.py
@@ -1,4 +1,5 @@
 from controller import Robot
+from controller import Motor
 
 
 class res_man:
@@ -9,8 +10,8 @@ class res_man:
         self.receiver = self.robot.getDevice("receiver")
         self.receiver.enable(10)
         
-        self.left = self.robot.getDevice('left wheel motor')
-        self.right = self.robot.getDevice('right wheel motor')
+        self.left:Motor = self.robot.getDevice('left wheel motor')
+        self.right:Motor = self.robot.getDevice('right wheel motor')
 
         self.left.setPosition(float("inf"))
         self.right.setPosition(float("inf"))
--- controllers/dave_controller/communicater.py
+++ controllers/dave_controller/communicater.py
@@ -1,6 +1,7 @@
 
 import json
-
+from dave_lib import Dave
+from Resource_manager import res_man
 def get_packets_and_update(receiver,update_on_receive):
     packet = None
     while receiver.getQueueLength() > 0:
@@ -9,3 +10,12 @@ def get_packets_and_update(receiver,update_on_receive):
     if(packet is not None):
         for func in update_on_receive:
             func(packet)
+
+
+def update_epuck(dave:Dave,res:res_man):
+    '''
+    Transfers data from dave object to epuck
+    '''
+    res.left.setVelocity(dave.left_v)
+    res.right.setVelocity(dave.right_v)
+    
