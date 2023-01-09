77cdea77fd8114d6c23a86b6c44c90b9e09c7070
Buddhi Wijenayake <wijenayakebuddhi34802@gmail.com>
2023-01-09 14:38:46 +0530

    distance-sensor new  function  implemented  to be tested
    distance-sensor new  function  implemented  to be tested

--- controllers/dave_controller/communicater.py
+++ controllers/dave_controller/communicater.py
@@ -2,6 +2,7 @@
 import json
 from dave_lib import Dave,NO_WALL
 from Resource_manager import res_man
+from distance_function import transform_sensor_reading
 def get_packets_and_update(receiver,update_on_receive):
     packet = None
     
@@ -17,13 +18,13 @@ def get_packets_and_update(receiver,update_on_receive):
 
 SCALING_FACTOR = 10
 THRESHOLD = 75 
-def transform_reading(reading):
+def transform_reading_old(reading):
     if(reading < THRESHOLD):
         return NO_WALL
     return SCALING_FACTOR/(reading-THRESHOLD)
 
 def positionofwalls(prox_sensors):
-    return list(map(transform_reading,(sensor.getValue() for sensor in prox_sensors)))
+    return list(map(transform_sensor_reading,(sensor.getValue() for sensor in prox_sensors)))
 
 def update_epuck(dave:Dave,res:res_man):
     '''
---
+++ controllers/dave_controller/distance_function.py
@@ -0,0 +1,49 @@
+from bisect import bisect_left
+LOOKUP = [(0, 4095),
+          (0.005, 2133.33),
+          (0.01, 1465.73),
+          (0.015, 601.46),
+          (0.02, 383.84),
+          (0.03, 234.93),
+          (0.04, 158.03),
+          (0.05, 120),
+          (0.06, 104.09),
+          (0.07, 67.19)]
+
+REVERSE_LOOKUP = [tuple(reversed(val)) for val in LOOKUP]
+
+
+def find_range(sensor_reading):
+    '''
+    return  max_val,min_val
+    '''
+    index_range_max = bisect_left(REVERSE_LOOKUP, sensor_reading)
+    index_range_min = index_range_max-1
+    max_val = REVERSE_LOOKUP[index_range_max]
+    min_val = REVERSE_LOOKUP[index_range_min]
+    return max_val, min_val
+
+
+MIN_READING = 68
+MAX_READING = 4094
+WALL_NOT_DETECTED_READING = float("inf")
+
+
+def interpolate(value_range, sensor_reading):
+    (r_max, d_max), (r_min, d_min) = value_range
+    r_sensor_reading = sensor_reading
+    sensor_distance = d_max - (r_sensor_reading-r_min) * \
+        (d_max-d_min)/(r_max-r_min)
+    return sensor_distance
+
+
+def transform_sensor_reading(sensor_reading):
+    if sensor_reading >= MAX_READING:
+        return 0
+    if sensor_reading <= MIN_READING:
+        return WALL_NOT_DETECTED_READING
+
+    return interpolate(
+        find_range(sensor_reading),
+        sensor_reading
+    )
