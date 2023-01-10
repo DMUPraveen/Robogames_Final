bf6cab0d99ab1965d9e2f5a2f48034886881aa8b
DMUPraveen <dmu.praveen@gmail.com>
2023-01-10 11:17:57 +0530

    Added version compatibility

--- controllers/dave_controller/communicater.py
+++ controllers/dave_controller/communicater.py
@@ -1,13 +1,18 @@
 
 import json
-from dave_lib import Dave,NO_WALL
+from dave_lib import Dave, NO_WALL
 from Resource_manager import res_man
 from distance_function import transform_sensor_reading
-def get_packets_and_update(receiver,update_on_receive):
+
+
+def get_packets_and_update(receiver, update_on_receive):
     packet = None
-    
+
     while receiver.getQueueLength() > 0:
-        packet = json.loads(receiver.getData())
+        data = receiver.getData()
+        if(type(data) != str):
+            data = data.decode('utf-8')
+        packet = json.loads(data)
         # print(packet)
         receiver.nextPacket()
     if(packet is not None):
@@ -16,21 +21,25 @@ def get_packets_and_update(receiver,update_on_receive):
         return True
     return False
 
+
 SCALING_FACTOR = 10
-THRESHOLD = 75 
+THRESHOLD = 75
+
+
 def transform_reading_old(reading):
     if(reading < THRESHOLD):
         return NO_WALL
     return SCALING_FACTOR/(reading-THRESHOLD)
 
-def positionofwalls(prox_sensors):
-    return list(map(transform_sensor_reading,(sensor.getValue() for sensor in prox_sensors)))
 
-def update_epuck(dave:Dave,res:res_man):
+def positionofwalls(prox_sensors):
+    return list(map(transform_sensor_reading, (sensor.getValue() for sensor in prox_sensors)))
+
+
+def update_epuck(dave: Dave, res: res_man):
     '''
     Transfers data from dave object to epuck
     '''
     res.left.setVelocity(dave.left_v)
     res.right.setVelocity(dave.right_v)
     dave.wall_dis = positionofwalls(res.prox_sensors)
-
