f4b1f9fa240ab8e597c72dc8aadc3300bc837fc5
DMUPraveen <dmu.praveen@gmail.com>
2023-01-14 18:44:06 +0530

    progress in the path planning Topomap_generation

--- controllers/dave_controller/path_planning.py
+++ controllers/dave_controller/path_planning.py
@@ -1,8 +1,9 @@
-from typing import Iterable, List, Tuple, Deque
+from typing import Iterable, List, Tuple, Deque, Set
 from dave_lib import Dave
 from Motion_Control_Class import Motion_Control
 from enum import Enum
 from collections import deque
+from Occupancy_grid import Cartesian_to_Grid
 
 
 class Point_Follow_States(Enum):
@@ -39,3 +40,45 @@ class Point_Follow:
             self.state = Point_Follow_States.FINISHED
             return
         self.motion_controller.set_target(*self.path_points.popleft())
+
+
+class TopoNode:
+    def __init__(self, index: int, position: Tuple[float, float]):
+        self.index = index
+        self.connections: Set[TopoNode] = set()
+        self.position = position
+
+    def __hash__(self) -> int:
+        return self.index
+
+
+class Node_Maker:
+    def __init__(self):
+        self.index = -1
+
+    def make_node(self, position: Tuple[float, float]):
+        self.index += 1
+        return TopoNode(self.index, position)
+
+
+class Topological_Map:
+    SAFETY_FACTOR = 10
+
+    def __init__(self, x_width, y_width, scale):
+        self.node_maker: Node_Maker = Node_Maker()
+        grid_width = int(x_width/scale+0.5)+self.SAFETY_FACTOR
+        grid_height = int(y_width/scale+0.5)+self.SAFETY_FACTOR
+        width = max(grid_height, grid_width)
+        origin_pos = width//2
+
+        self.cartesian_to_grid = Cartesian_to_Grid(
+            scale,
+            origin_pos,
+            origin_pos
+        )
+        self.topo_grid: List[List[List[TopoNode]]] = [
+            [[] for _ in range(width)] for _ in range(width)
+        ]
+
+    def construct_topo_map(self, dave: Dave):
+        pass
