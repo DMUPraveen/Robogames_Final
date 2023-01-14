8e427dc17f8554b6952bd6eec9c37329db683e9c
DMUPraveen <dmu.praveen@gmail.com>
2023-01-14 10:51:36 +0530

    Finished writing dda algorithm to tested

--- controllers/dave_controller/dda_algo.py
+++ controllers/dave_controller/dda_algo.py
@@ -4,9 +4,19 @@ import numpy as np
 
 def perform_dda(direction_vector: Tuple[float, float],
                 start_scaled_to_grid_units: Tuple[float, float],
-                stopping_condition: Callable[[int, int], bool],
-                on_new_cell: Callable[[int, int], None],
-                maxdistnace: float) -> None:
+                stopping_condition: Callable[[Tuple[int, int], float], bool],
+                on_new_cell: Callable[[Tuple[int, int]], None],
+                ) -> Tuple[Tuple[int, int], float]:
+    '''
+    performs the dda algorithm on a grid
+
+    direction_vector: vector giving the direction of the ray -- does not need to be a unit vector 
+    start_scaled_to_grid_units: (x,y) values sclaed such that one grid cell is 1 unit
+    stopping_condition: condition to be stopped Takes in (x,y), distance
+    on_new_cell: what to do on a new cell (will be performed to the stopping cell as well)
+
+    returns: the cell which the algorithm stopped and the distance traveled thus far
+    '''
     np_dir_vec = np.array(direction_vector)
     dir_unit_vec = np_dir_vec/np.linalg.norm(np_dir_vec)
     # distance traveled along the direction for one unit in x
@@ -14,7 +24,7 @@ def perform_dda(direction_vector: Tuple[float, float],
     # distnace traveled along the direction for one unit in y
     Sy: float = abs(1/dir_unit_vec[1])
     # The tile that is currently inhabited
-    current_tile = tuple(map(int, start_scaled_to_grid_units))
+    current_tile = list(map(int, start_scaled_to_grid_units))
 
     x_ray_length: float = 0.0
     y_ray_length: float = 0.0
@@ -35,5 +45,18 @@ def perform_dda(direction_vector: Tuple[float, float],
 
     stop_flag = False
     distance: float = 0.0
+    tuple_current_tile = tuple(current_tile)
     while(not stop_flag):
-        pass
+        tuple_current_tile = tuple(current_tile)
+        if(x_ray_length < y_ray_length):
+            current_tile[0] += step[0]
+            distance = x_ray_length
+            x_ray_length += Sx
+        else:
+            current_tile[1] += step[1]
+            distance = x_ray_length
+            y_ray_length += Sy
+        on_new_cell(tuple_current_tile)
+        if(stopping_condition(tuple_current_tile, distance)):
+            stop_flag = True
+    return tuple_current_tile, distance
