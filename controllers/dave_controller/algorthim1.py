800cc33e6dc4812a7ff485b9f53c870beb81835e
DMUPraveen <dmu.praveen@gmail.com>
2023-01-15 10:55:37 +0530

    Completed target reacher to be tested

---
+++ controllers/dave_controller/algorthim1.py
@@ -0,0 +1,143 @@
+from typing import Iterable, List, Tuple, Deque, Set, Dict, Optional
+from dave_lib import Dave, update_dave_pose, update_environment, Environment
+from Occupancy_grid import Occupancy_Grid, Cartesian_to_Grid, Mapper, get_true_distance_with_maximum_free_distance
+from Motion_Control_Class import Motion_Control
+from path_planning import Point_Follow, Point_Follow_States, Topological_Map, Reachability_Checker, find_best_path_possible, transform_node_list_to_point_follow_list, Dashability_Checker
+from wall_following import attempt2_left_wall_following, attempt2_right_wall_following
+from enum import Enum
+import numpy as np
+
+
+class Target_Reacher_State(Enum):
+    IDLE = 0
+    FIND_PATH = 1
+    PATH_FOLLOWING = 2
+    DASHING = 3
+    LEFT_WALL_FOLLOWING = 4
+    RIGHT_WALL_FOLLOWING = 5
+    FINISHED = 6
+
+
+class Target_Reacher:
+    def __init__(self, occupancy_grid: Occupancy_Grid, dashability_checker: Dashability_Checker,
+                 topo_map: Topological_Map, dave: Dave, motion_controller: Motion_Control, point_follow: Point_Follow):
+        self.state: Target_Reacher_State = Target_Reacher_State.IDLE
+        self.path_to_follow: Optional[List[Tuple[float, float]]] = None
+        self.topo_map = topo_map
+        self.occupancy_grid = occupancy_grid
+        self.dave = dave
+        self.target: Optional[Tuple[float, float]] = None
+        self.motion_controller = motion_controller
+        self.point_follow = point_follow
+        self.dashability_checker = dashability_checker
+
+        self.funs_to_run = {
+            Target_Reacher_State.IDLE: self.on_idle,
+            Target_Reacher_State.DASHING: self.dashing,
+            Target_Reacher_State.FIND_PATH: self.on_find_path,
+            Target_Reacher_State.PATH_FOLLOWING: self.path_following,
+            Target_Reacher_State.LEFT_WALL_FOLLOWING: self.left_wall_following,
+            Target_Reacher_State.RIGHT_WALL_FOLLOWING: self.right_wall_following,
+        }
+
+    def reset(self):
+        if(self.path_to_follow is not None):
+            self.path_to_follow.clear()
+        self.target = None
+        self.state = Target_Reacher_State.IDLE
+
+    def set_target_and_reset(self, target: Tuple[float, float]):
+        self.reset()
+        self.target = target
+
+    def on_idle(self):
+        if(self.target is not None):
+            self.state = Target_Reacher_State.FIND_PATH
+
+    def go_to_dashing(self):
+        if(self.target is None):
+            self.reset()
+            return
+        self.motion_controller.set_target(self.target[0], self.target[1])
+
+    def on_find_path(self):
+        if(self.target is None):
+            self.reset()
+            return
+        current_pos = (self.dave.x, self.dave.y)
+        best_path = find_best_path_possible(
+            self.topo_map,
+            current_pos,
+            self.target,
+        )
+        if(best_path is None):
+            self.go_to_dashing()
+            return
+        self.state = Target_Reacher_State.PATH_FOLLOWING
+        self.path_to_follow = transform_node_list_to_point_follow_list(
+            best_path)
+        self.point_follow.terminate_current_run_and_set_path(
+            self.path_to_follow)
+        self.point_follow.start_current_path()
+
+    def path_following(self):
+        self.point_follow.run(self.dave)
+        if(self.point_follow.state == Point_Follow_States.FINISHED):
+            self.go_to_dashing()
+
+    def go_to_wall_following(self):
+        if(self.target is None):
+            self.reset()
+            return
+        front_vector = self.dave.get_front_facing_vector().flatten()
+        target_vector = np.array(self.target) - \
+            np.array((self.dave.x, self.dave.y))  # type: ignore
+        cross_product_value = float(np.cross(front_vector, target_vector))
+        if(cross_product_value < 0):
+            self.state = Target_Reacher_State.RIGHT_WALL_FOLLOWING
+        else:
+            self.state = Target_Reacher_State.LEFT_WALL_FOLLOWING
+
+    def dashing(self):
+        self.motion_controller.go_to_position(
+            self.dave, self.point_follow.first_turn_threshold, self.point_follow.linear_threshold)
+        if(self.target is None):
+            self.reset()
+            return
+        can_dash = self.dashability_checker.check_dashability(
+            self.dave.x, self.dave.y,
+            self.target[0], self.target[1]
+        )
+        if(not can_dash):
+            self.go_to_wall_following()
+
+    def left_wall_following(self):
+        if(self.target is None):
+            self.reset()
+            return
+
+        attempt2_left_wall_following(self.dave)
+        can_dash = self.dashability_checker.check_dashability(
+            self.dave.x, self.dave.y,
+            self.target[0], self.target[1]
+        )
+        if(can_dash):
+            self.go_to_dashing()
+
+    def right_wall_following(self):
+        if(self.target is None):
+            self.reset()
+            return
+
+        attempt2_right_wall_following(self.dave)
+        can_dash = self.dashability_checker.check_dashability(
+            self.dave.x, self.dave.y,
+            self.target[0], self.target[1]
+        )
+        if(can_dash):
+            self.go_to_dashing()
+
+    def run(self):
+        if(self.state == Target_Reacher_State.FINISHED):
+            return
+        self.funs_to_run[self.state]()
---
+++ controllers/dave_controller/testing_algorithm1.py
@@ -0,0 +1,109 @@
+
+import pygame
+# from Graphic_Engine import Graphic_Engine, calculate_origin
+from Resource_manager import res_man
+from dave_lib import Dave, update_dave_pose, update_environment, Environment
+from communicater import get_packets_and_update, update_epuck
+from remote_control import control_dave_via_keyboard, actions
+from New_graphic_Engine import Graphic_Engine, draw_dave, draw_grid_view, tracking_grid_view
+from Occupancy_grid import Occupancy_Grid, Cartesian_to_Grid, Mapper, get_true_distance_with_maximum_free_distance
+from Motion_Control_Class import Motion_Control
+from path_planning import Point_Follow, Point_Follow_States, Topological_Map, Reachability_Checker, find_best_path_possible, transform_node_list_to_point_follow_list, Dashability_Checker
+from algorthim1 import Target_Reacher
+
+
+def main():
+    ############################ Resource Management #####################################
+    res = res_man()
+    dave = Dave()
+    env = Environment()
+    OCCUPANCY_GRID_WIDTH = 4000
+    OCCUPANCY_GRID_HEIGHT = 4000
+    OCCUPANCY_GRID_SCALE = 0.004
+    occupancy_grid = Occupancy_Grid(
+        OCCUPANCY_GRID_WIDTH, OCCUPANCY_GRID_HEIGHT)
+    cart_to_grid_pos_converter = Cartesian_to_Grid(
+        OCCUPANCY_GRID_SCALE, OCCUPANCY_GRID_WIDTH//2, OCCUPANCY_GRID_HEIGHT//2)
+    obstacle_cell_determiner = get_true_distance_with_maximum_free_distance(
+        0.06, 0.04)
+    mapper = Mapper(occupancy_grid, cart_to_grid_pos_converter,
+                    obstacle_cell_determiner, 0.001)
+    motion_controller = Motion_Control(res.timestep)
+    #######################################################################################
+
+    ############################ Setting up the Resources and Communication################
+
+    def update_dave_on_packet(packet): return update_dave_pose(packet, dave)
+
+    def update_env_on_packet(packet): return update_environment(packet, env)
+
+    update_on_receive = [update_dave_on_packet, update_env_on_packet]
+    #######################################################################################
+
+    ########################### Debug Visualization Setup ##################################
+    vis = Graphic_Engine()
+    SCREEN_WIDTH = 400
+    SCREEN_HEIGHT = 400
+    vis.initialize(SCREEN_WIDTH, SCREEN_HEIGHT)
+
+    ROBOT_RADIUS = 20
+    ROBOT_OFFSET = ROBOT_RADIUS*2
+
+    def robot_visualization(screen: pygame.Surface): return draw_dave(
+        screen, dave, ROBOT_RADIUS, SCREEN_WIDTH-ROBOT_OFFSET, SCREEN_HEIGHT-ROBOT_OFFSET)
+
+    def render_grid_subset(screen: pygame.Surface):
+        # print(occupancy_grid.grid[1900:2100, 1450:1550])
+        draw_grid_view(
+            screen, occupancy_grid.grid[1900:2100, 1450:1550], 4, (0, 0))
+
+    def render_grid_tracking_subset(screen: pygame.Surface):
+        tracking_position = cart_to_grid_pos_converter(dave.x, dave.y)
+        grid_view = tracking_grid_view(tracking_position, occupancy_grid, 100)
+        draw_grid_view(screen, grid_view, 4, (0, 0))
+
+    all_visualizations = [
+        Graphic_Engine.clear_screen,
+        robot_visualization,
+        render_grid_tracking_subset,
+
+    ]
+    ######################################################################################
+
+    ############################Code for Path Planning####################################
+    point_follower = Point_Follow(motion_controller, 0.1, 0.003)
+
+    reachability_checker = Reachability_Checker(
+        occupancy_grid, cart_to_grid_pos_converter)
+    topo_map = Topological_Map(
+        5.0, 5.0, 0.1, 0.03, 2, reachability_checker, 0.01)
+
+    dashability_checker = Dashability_Checker(
+        occupancy_grid, cart_to_grid_pos_converter)
+    target_reacher = Target_Reacher(
+        occupancy_grid,
+        dashability_checker,
+        topo_map,
+        dave,
+        motion_controller,
+        point_follower,
+    )
+    target_reacher.set_target_and_reset((-1.431, -1.498))
+    ######################################################################################
+
+    ############################## Main Loop #############################################
+
+    while res.robot.step(res.timestep) != -1:
+        packet_recieved = get_packets_and_update(
+            res.receiver, update_on_receive)
+        update_epuck(dave, res)
+        mapper.mapping_with_dda(dave)
+        vis.run(all_visualizations)
+        # print(point_follower.state)
+        target_reacher.run()
+        topo_map.construct_topo_map(dave)
+        # print(dave)
+        # control_dave_via_keyboard(res.keyboard, dave)
+        # print(dave.get_distances()[0])
+        # print(dave)
+    ######################################################################################
